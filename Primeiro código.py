6c294dca03045fb8525d53c6d6da7b7b7fbb3880
Matheus1300 <70530639+Matheus1300@users.noreply.github.com>
2020-10-02 15:26:51 -0300

    Add files via upload
    Adicionado o primeiro arquivo

---
+++ Primeiro código.py
@@ -0,0 +1,7 @@
+# Seja bem vindo ao novo projeto!
+
+print('Hello, World!') # Esta linha irá fazer o computador escrever "Hello, World!"
+
+#   nome = str(input('Qual seu nome? '))         Códigos exemplares, esse irá
+#   print(nome)                                  perguntar seu nome e digitar na tela.
+#
